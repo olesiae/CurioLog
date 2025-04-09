5d3fbdf5d97d33445f076fa0481dac58a25b2706
Olesia Efimova <lesichh@gmail.com>
2025-04-09 14:36:05 +0200

    Initial Commit

---
+++ CurioLog/CurioLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurioLogApp.swift
+//  CurioLog
+//
+//  Created by Олеся Ефимова on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CurioLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurioLogTests/CurioLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurioLogTests.swift
+//  CurioLogTests
+//
+//  Created by Олеся Ефимова on 9/4/25.
+//
+
+import Testing
+@testable import CurioLog
+
+struct CurioLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
